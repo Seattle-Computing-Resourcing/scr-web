0d684dc0988f8ed9a5ec68d46c9c4313882981a8
ruben <jing.ruben@gmail.com>
2024-12-23 16:25:54 -0800

    working favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './components/navbar/Navbar.jsx'
-import Favicon from 'react-favicon';
 
 const App = () => {
   const currentTheme = localStorage.getItem("currentTheme");
@@ -10,11 +9,8 @@ const App = () => {
     localStorage.setItem("currentTheme", theme);
   }, [theme])
 
-  const faviconUrl = theme == "light" ? "%PUBLIC_URL%/favicon.ico" : "%PUBLIC_URL%/favicon_light.ico";
-
   return (
     <div className = {`container ${theme}`}>
-      <Favicon url={faviconUrl} />
       <Navbar theme={theme} setTheme={setTheme}/>
     </div>
   )
