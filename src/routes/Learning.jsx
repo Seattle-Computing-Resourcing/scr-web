37a6775598004ee0da6d61a6d3e015f8cc53713c
ruben <jing.ruben@gmail.com>
2024-12-25 14:44:50 -0800

    make buttons for learning page

--- src/routes/Learning.jsx
+++ src/routes/Learning.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '.././components/navbar/Navbar.jsx'
 import './Learning.css'
+import Button from '../components/button/Button.jsx';
 
 const Learning = () => {
     const currentTheme = localStorage.getItem("currentTheme");
@@ -18,6 +19,11 @@ const Learning = () => {
     return (
       <div className = {`container ${theme}`}>
         <Navbar theme={theme} setTheme={setTheme}/>
+
+        <div className='learning-hero'>
+          <h1>Seattle Computing Resourcing</h1>
+          <Button buttonStyle='btn--outline' buttonSize='btn--large'>Start Learning</Button>
+        </div>
       </div>
     )
 }
