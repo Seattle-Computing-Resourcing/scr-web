3fcf370bd9af035655a29286bb3d0ef86393e50c
ruben <jing.ruben@gmail.com>
2024-12-26 18:02:24 -0800

    change of plan: learning will be on another site

--- src/router.jsx
+++ src/router.jsx
@@ -4,8 +4,6 @@ import Learning from './routes/Learning.jsx';
 import Exercises from './routes/Exercises.jsx';
 import Contests from './routes/Contests.jsx';
 import Contact from './routes/Contact.jsx';
-import Test from "./content/test.mdx";
-import Test2 from "./content/test2.mdx";
 
 export const router = createBrowserRouter([
   { path: "/", element: <App /> },
@@ -13,6 +11,4 @@ export const router = createBrowserRouter([
   { path: "/exercises", element: <Exercises /> },
   { path: "/contests", element: <Contests /> },
   { path: "/contact", element: <Contact /> },
-  { path: "/learning/test", element: <Test /> },
-  { path: "/learning/test2", element: <Test2 /> },
 ]);
