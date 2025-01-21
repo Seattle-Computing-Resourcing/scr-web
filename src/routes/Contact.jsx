21d52cbd1b67ce8048ae22fc69e3c71f3fae3b7f
ruben <jing.ruben@gmail.com>
2025-01-21 12:13:35 -0800

    fix another thing

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -11,11 +11,11 @@ const Contact = () => {
     setResult("Sending....");
     const formData = new FormData(event.target);
 
-    const access_key = import.meta.env.VITE_ACCESS_KEY;
+    // const access_key = import.meta.env.VITE_ACCESS_KEY;
 
     // console.log(access_key);
 
-    formData.append("access_key", access_key);
+    formData.append("access_key", import.meta.env.VITE_ACCESS_KEY);
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
